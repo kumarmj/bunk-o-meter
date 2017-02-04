06e877ffd5228405b5341fe59bfee31449b1838b
Manoj Kumar <nithmanoj@gmail.com>
2017-02-04 18:54:15 +0530

    app shell

---
+++ js/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from "react-dom"
+import { Router, Route, hashHistory } from 'react-router'
+import App from './app.js'
+
+ReactDOM.render(
+ <App/>,
+ document.getElementById('bunko')
+);
