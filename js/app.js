cc8b0e821193c66a721f4a7c26aac1de5db0bac8
Manoj Kumar <nithmanoj@gmail.com>
2017-02-04 18:54:15 +0530

    app shell

--- js/app.js
+++ js/app.js
@@ -1,67 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
-    return 
-<button id="demo-menu-lower-right"
-        class="mdl-button mdl-js-button mdl-button--icon">
-  <i class="material-icons">more_vert</i>
-</button>
-
-<ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
-    for="demo-menu-lower-right">
-  <li class="mdl-menu__item">Some Action</li>
-  <li class="mdl-menu__item">Another Action</li>
-  <li disabled class="mdl-menu__item">Disabled Action</li>
-  <li class="mdl-menu__item">Yet Another Action</li>
-</ul>
-    <!-- Colored FAB button -->
-<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
-  <i class="material-icons">add</i>
-</button>
-    <!-- Simple header with scrollable tabs. -->
-<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-  <header class="mdl-layout__header">
-    <div class="mdl-layout__header-row">
-      <!-- Title -->
-      <span class="mdl-layout-title">Title</span>
-    </div>
-    <!-- Tabs -->
-    <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
-      <a href="#scroll-tab-1" class="mdl-layout__tab is-active">Bunks</a>
-      <a href="#scroll-tab-2" class="mdl-layout__tab">Notices</a>
-      <a href="#scroll-tab-3" class="mdl-layout__tab">Results</a>
-      <a href="#scroll-tab-4" class="mdl-layout__tab">Tab 4</a>
-      <a href="#scroll-tab-5" class="mdl-layout__tab">Tab 5</a>
-      <a href="#scroll-tab-6" class="mdl-layout__tab">Tab 6</a>
-    </div>
-  </header>
-  <div class="mdl-layout__drawer">
-    <span class="mdl-layout-title">Title</span>
-  </div>
-  <main class="mdl-layout__content">
-    <section class="mdl-layout__tab-panel is-active" id="scroll-tab-1">
-      <div class="page-content"><!-- Your content goes here --></div>
-    </section>
-    <section class="mdl-layout__tab-panel" id="scroll-tab-2">
-      <div class="page-content"><!-- Your content goes here --></div>
-    </section>
-    <section class="mdl-layout__tab-panel" id="scroll-tab-3">
-      <div class="page-content"><!-- Your content goes here --></div>
-    </section>
-    <section class="mdl-layout__tab-panel" id="scroll-tab-4">
-      <div class="page-content"><!-- Your content goes here --></div>
-    </section>
-    <section class="mdl-layout__tab-panel" id="scroll-tab-5">
-      <div class="page-content"><!-- Your content goes here --></div>
-    </section>
-    <section class="mdl-layout__tab-panel" id="scroll-tab-6">
-      <div class="page-content"><!-- Your content goes here --></div>
-    </section>
-  </main>
-</div>;
+    return (
+            <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+              <header className="mdl-layout__header">
+                <div className="mdl-layout__header-row">
+                  <span className="mdl-layout-title">Bunk-o-Meter</span>
+                </div>
+                <div className="mdl-layout__tab-bar mdl-js-ripple-effect">
+                  <a href="#scroll-tab-1" className="mdl-layout__tab is-active">Bunks</a>
+                  <a href="#scroll-tab-2" className="mdl-layout__tab">Notices</a>
+                  <a href="#scroll-tab-3" className="mdl-layout__tab">Results</a>
+                </div>
+              </header>
+              <div className="mdl-layout__drawer">
+                <span className="mdl-layout-title">Menu</span>
+              </div>
+              <main className="mdl-layout__content">
+                
+                <section className="mdl-layout__tab-panel" id="scroll-tab-2">
+                  <div className="page-content">This is tab2</div>
+                </section>
+                <section className="mdl-layout__tab-panel" id="scroll-tab-3">
+                  <div className="page-content">This is tab3</div>
+                </section>
+              </main>
+            </div>
+      );
   }
 }
-
-module.exports = App
